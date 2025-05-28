35103b708319893efbdac845feca8611284c2e23
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2025-05-28 08:47:28 +0500

    trying to fix deployment issue

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 // import router from './Pages';
-import router from './Pages/Router';
+import router from './Pages/Router.jsx';
 // import './App.css';
 
 createRoot(document.getElementById('root')).render(
