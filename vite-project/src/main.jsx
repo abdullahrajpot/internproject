3b2e86e188cc23ac5c509ad7db2cb892421bdae3
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2025-05-28 09:07:36 +0500

    fixing

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 // import router from './Pages';
-import Router from './Pages/Router.jsx';
+import Router from './Pages/Router';
 // import './App.css';
 
 createRoot(document.getElementById('root')).render(
